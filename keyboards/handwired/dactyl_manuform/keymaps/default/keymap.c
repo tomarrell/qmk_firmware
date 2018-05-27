8f4826c98282d6cce826198c2435abc0fc5351fb
Tom Arrell <tom.arrell@gmail.com>
2018-05-28 02:36:33 +1200

    Programmed Am keyboard layout

--- keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
@@ -35,46 +35,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------'
  * |  |>  | LEFT | DOWN |  UP  | RGHT |
  * '----------------------------------'
- *                                           ,------,------,
- *                                           |   $  |   %  |
- *                                    ,------|------|------|
- *                                    |      |      |  DEL |
- *                                    | BKSP | SHFT |------|
- *                                    |      |      |  CMD |
- *                                    '------'------'------'
- *
- *
- * -= RIGHT HAND =-
- *
- *               ,-----------------------------------------,
- *               |   6  |   7  |   8  |   9  |   0  |   =  |
- *               |-----------------------------------------|
- *               |   Y  |   U  |   I  |   O  |   P  |   -  |
- *               |------+------+------+------+-------------|
- *               |   H  |   J  |   K  |   L  |   ;  |   '  |
- *               |-----------------------------------------|
- *               |   N  |   M  |   ,  |   .  |   /  | RSHFT|
- *               '-----------------------------------------|
- *                      |   [  |   ]  |   \  |  FWD |  BCK |
- *                      '----------------------------------'
- * ,------,------,
- * |   &  |   !  |
- * |------|------|------,
- * |  ALT |      |      |
- * |------| SPCE | ENTR |
- * | CTRL |      |      |
- * '------'------'------'
  *
  */
 
 [_BASE] = KEYMAP( \
-  KC_GRV,    KC_1, KC_2, KC_3, KC_4, KC_5,                                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,     \
-  KC_TAB,    KC_Q, KC_W, KC_E, KC_R, KC_T,                                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,    \
-  KC_ESCAPE, KC_A, KC_S, KC_D, KC_F, KC_G,                                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    \
-  KC_HOME,   KC_Z, KC_X, KC_C, KC_V, KC_B,                                    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,    \
-  KC_MPLY,   KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,                                        KC_LBRC, KC_RBRC, KC_BSLS, KC_WFWD, KC_WBAK,    \
-                                     KC_DLR, KC_PERC, KC_DEL,       KC_LALT,  KC_AMPR, KC_EXLM,                                        \
-                                     KC_BSPC, KC_LSFT, KC_LGUI,      KC_LCTRL, KC_SPC,  KC_ENT                                          \
+  KC_GRV,    KC_1,    KC_2,     KC_3,    KC_4,  KC_5,  KC_6,  KC_X,    KC_7, KC_8,     KC_9,    KC_0,    KC_MINS, KC_PLUS, KC_BSPC, KC_DEL,  \
+  KC_TAB,    KC_Q,    KC_W,     KC_E,    KC_R,  KC_T,  KC_NO, KC_X,    KC_Y, KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
+  KC_ESCAPE, KC_A,    KC_S,     KC_D,    KC_F,  KC_G,  KC_NO, KC_X,    KC_H, KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_UNDO, \
+  KC_HOME,   KC_COMM, KC_Z,     KC_X,    KC_C,  KC_V,  KC_B,  KC_X,    KC_N, KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_COPY, \
+  KC_LCTRL,  KC_LGUI, KC_LCTRL, KC_LALT, KC_NO, KC_NO, KC_NO, KC_X,    KC_1, KC_SPACE, KC_RALT, KC_VOLD, KC_VOLU, KC_LEFT, KC_DOWN, KC_RIGHT \
 )
 
 };
