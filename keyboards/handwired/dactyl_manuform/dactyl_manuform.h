eb9779e935172265327294c838e2d8168286c571
Tom Arrell <tom.arrell@gmail.com>
2018-07-09 05:06:27 +1200

    Almost working Am

--- keyboards/handwired/dactyl_manuform/dactyl_manuform.h
+++ keyboards/handwired/dactyl_manuform/dactyl_manuform.h
@@ -9,33 +9,33 @@
 #ifdef USE_I2C
 #include <stddef.h>
 #ifdef __AVR__
-	#include <avr/io.h>
-	#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 #endif
 #endif
 
 //void promicro_bootloader_jmp(bool program);
 
 #define KEYMAP( \
-        L00, L01, L02, L03, L04, L05, L06, L07,      R00, R01, R02, R03, R04, R05, R06, R07,         \
-        L10, L11, L12, L13, L14, L15, L16, L17,      R10, R11, R12, R13, R14, R15, R16, R17,         \
-        L20, L21, L22, L23, L24, L25, L26, L27,      R20, R21, R22, R23, R24, R25, R26, R27,         \
-        L30, L31, L32, L33, L34, L35, L36, L37,      R30, R31, R32, R33, R34, R35, R36, R37,         \
-        L40, L41, L42, L43, L44, L45, L46, L47,      R40, R41, R42, R43, R44, R45, R46, R47          \
-                                                                                                     \
-	)                                                                                                  \
-	{                                                                                                  \
-		{ L00, L01, L02, L03, L04, L05, L06, L07 },                                                      \
-		{ L10, L11, L12, L13, L14, L15, L16, L17 },                                                      \
-		{ L20, L21, L22, L23, L24, L25, L26, L27 },                                                      \
-		{ L30, L31, L32, L33, L34, L35, L36, L37 },                                                      \
-		{ L40, L41, L42, L43, L44, L45, L46, L47 },                                                      \
-\
-		{ L00, L01, L02, L03, L04, L05, L06, L07 },                                                      \
-		{ L10, L11, L12, L13, L14, L15, L16, L17 },                                                      \
-		{ L20, L21, L22, L23, L24, L25, L26, L27 },                                                      \
-		{ L30, L31, L32, L33, L34, L35, L36, L37 },                                                      \
-		{ L40, L41, L42, L43, L44, L45, L46, L47 }                                                       \
-	}
+    L00, L01, L02, L03, L04, L05, L06,      R00, R01, R02, R03, R04, R05, R06, R07,              \
+    L10, L11, L12, L13, L14, L15,           R10, R11, R12, R13, R14, R15, R16, R17,              \
+    L20, L21, L22, L23, L24, L25,           R20, R21, R22, R23, R24, R25, R26, R27,              \
+    L30, L31, L32, L33, L34, L35, L36,      R30, R31, R32, R33, R34, R35, R36, R37,              \
+    L40, L41, L42, L43, L44,                     R40, R41, R42, R43, R44, R45, R46               \
+    \
+    )                                                                                                  \
+{                                                                                                  \
+  { L00, L01, L02, L03, L04, L05, L06,                   KC_NO },                                                      \
+  { L10, L11, L12, L13, L14, L15,                 KC_NO, KC_NO },                                                      \
+  { L20, L21, L22, L23, L24, L25,                 KC_NO, KC_NO },                                                      \
+  { L30, L31, L32, L33, L34, L35, L36,                   KC_NO },                                                      \
+  { L40, L41, L42, L43, L44,               KC_NO, KC_NO, KC_NO },                                                      \
+  \
+  { R00, R01, R02, R03, R04, R05, R06, R07 },                                                      \
+  { R10, R11, R12, R13, R14, R15, R16, R17 },                                                      \
+  { R20, R21, R22, R23, R24, R25, R26, R27 },                                                      \
+  { R30, R31, R32, R33, R34, R35, R36, R37 },                                                      \
+  { KC_NO, R40, R41, R42, R43, R44, R45, R46 }                                                       \
+}
 
 #endif
--- keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
@@ -39,11 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_BASE] = KEYMAP( \
-  KC_GRV,    KC_1,    KC_2,     KC_3,    KC_4,  KC_5,  KC_6,  KC_X,    KC_7, KC_8,     KC_9,    KC_0,    KC_MINS, KC_PLUS, KC_BSPC, KC_DEL,  \
-  KC_TAB,    KC_Q,    KC_W,     KC_E,    KC_R,  KC_T,  KC_NO, KC_X,    KC_Y, KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
-  KC_ESCAPE, KC_A,    KC_S,     KC_D,    KC_F,  KC_G,  KC_NO, KC_X,    KC_H, KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_UNDO, \
-  KC_HOME,   KC_COMM, KC_Z,     KC_X,    KC_C,  KC_V,  KC_B,  KC_X,    KC_N, KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_COPY, \
-  KC_LCTRL,  KC_LGUI, KC_LCTRL, KC_LALT, KC_NO, KC_NO, KC_NO, KC_X,    KC_1, KC_SPACE, KC_RALT, KC_VOLD, KC_VOLU, KC_LEFT, KC_DOWN, KC_RIGHT \
+  KC_ESCAPE, KC_1,    KC_2,    KC_3,  KC_4,     KC_5, KC_6,            KC_7,     KC_8,     KC_9,     KC_0,   KC_MINS,   KC_PLUS,  KC_INS,  KC_BSPC, \
+  KC_TAB,    KC_Q,    KC_W,    KC_E,  KC_R,     KC_T,                  KC_Y,     KC_U,     KC_I,     KC_O,   KC_P,      KC_LBRC,  KC_RBRC, KC_BSLASH, \
+  KC_CAPS,   KC_A,    KC_S,    KC_D,  KC_F,     KC_G,                  KC_H,     KC_J,     KC_K,     KC_L,   KC_SCOLON, KC_QUOTE, KC_DEL,  KC_5, \
+  KC_HOME,   KC_LSFT, KC_Z,    KC_X,  KC_C,     KC_V, KC_B,            KC_N,     KC_M,     KC_COMMA, KC_DOT, KC_SLASH,  KC_PGDN,  KC_UP,   KC_PGUP, \
+  KC_LCTRL,  KC_LGUI, KC_LALT, KC_1,  KC_LSFT,                                   KC_SPACE, KC_1,     KC_2,   KC_3,      KC_LEFT,  KC_DOWN, KC_RIGHT
 )
 
 };
