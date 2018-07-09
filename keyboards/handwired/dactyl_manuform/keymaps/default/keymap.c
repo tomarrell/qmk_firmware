073684b70b3c6eb66f321a2ce995c262ac2e52a1
Tom Arrell <tom.arrell@gmail.com>
2018-07-10 04:15:29 +1200

    Am keyboard fully working

--- keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/keymaps/default/keymap.c
@@ -39,11 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_BASE] = KEYMAP( \
-  KC_ESCAPE, KC_1,    KC_2,    KC_3,  KC_4,     KC_5, KC_6,            KC_7,     KC_8,     KC_9,     KC_0,   KC_MINS,   KC_PLUS,  KC_INS,  KC_BSPC, \
-  KC_TAB,    KC_Q,    KC_W,    KC_E,  KC_R,     KC_T,                  KC_Y,     KC_U,     KC_I,     KC_O,   KC_P,      KC_LBRC,  KC_RBRC, KC_BSLASH, \
-  KC_CAPS,   KC_A,    KC_S,    KC_D,  KC_F,     KC_G,                  KC_H,     KC_J,     KC_K,     KC_L,   KC_SCOLON, KC_QUOTE, KC_DEL,  KC_5, \
-  KC_HOME,   KC_LSFT, KC_Z,    KC_X,  KC_C,     KC_V, KC_B,            KC_N,     KC_M,     KC_COMMA, KC_DOT, KC_SLASH,  KC_PGDN,  KC_UP,   KC_PGUP, \
-  KC_LCTRL,  KC_LGUI, KC_LALT, KC_1,  KC_LSFT,                                   KC_SPACE, KC_1,     KC_2,   KC_3,      KC_LEFT,  KC_DOWN, KC_RIGHT
+  KC_ESCAPE, KC_1,    KC_2,    KC_3,   KC_4,    KC_5, KC_6,            KC_7,     KC_8,     KC_9,     KC_0,    KC_MINS,   KC_PLUS,  KC_DEL,   KC_BSPC, \
+  KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,                  KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,      KC_LBRC,  KC_RBRC,  KC_BSLASH, \
+  KC_CAPS,   KC_A,    KC_S,    KC_D,   KC_F,    KC_G,                  KC_H,     KC_J,     KC_K,     KC_L,    KC_SCOLON, KC_QUOTE, KC_ENTER, KC_END, \
+  KC_LSFT,  KC_PSCR, KC_Z,    KC_X,   KC_C,    KC_V, KC_B,            KC_N,     KC_M,     KC_COMMA, KC_DOT,  KC_SLASH,  KC_PGUP,  KC_UP,    KC_PGDN, \
+  KC_LCTRL,  KC_LGUI, KC_LALT, KC_INS, KC_LSFT,                                  KC_SPACE, KC_MUTE,  KC_VOLU, KC_VOLD,   KC_LEFT,  KC_DOWN,  KC_RIGHT
 )
 
 };
